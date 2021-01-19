245c4410a5d92c9647053cdb76d66de5826d43f2
CrazySean007 <shuaim@usc.edu>
2021-01-18 18:19:20 -0800

    upload the files on github

---
+++ public/precache-manifest.eddd4fb5d8a82789bc904b696edc6814.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddff7224ea45efc15f6ef3142a3f8d27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c60309b0dad9be18848",
+    "url": "/static/css/2.2d34248b.chunk.css"
+  },
+  {
+    "revision": "ee0c65ca5818ce103534",
+    "url": "/static/css/main.bebafb58.chunk.css"
+  },
+  {
+    "revision": "1c60309b0dad9be18848",
+    "url": "/static/js/2.70687bd3.chunk.js"
+  },
+  {
+    "revision": "417340539b8d83a7f891410b0079cc98",
+    "url": "/static/js/2.70687bd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee0c65ca5818ce103534",
+    "url": "/static/js/main.ab13a1b1.chunk.js"
+  },
+  {
+    "revision": "b4d41bbb8d28d5db8699",
+    "url": "/static/js/runtime-main.d6fb4cfd.js"
+  },
+  {
+    "revision": "ab45504baf89be7c7792b4426c4a6594",
+    "url": "/static/media/bg.ab45504b.jpg"
+  },
+  {
+    "revision": "19fff91776e5955eff158475f7c9dd93",
+    "url": "/static/media/cloudy.19fff917.png"
+  },
+  {
+    "revision": "ba1f87ecbc18b2f0a8b6e29e8676ac82",
+    "url": "/static/media/logo.ba1f87ec.png"
+  },
+  {
+    "revision": "3921549a6ac1ef827623fda7ddaedac9",
+    "url": "/static/media/p-cloudy.3921549a.png"
+  },
+  {
+    "revision": "65353ec2a272b1acd3917db37927085b",
+    "url": "/static/media/rain.65353ec2.png"
+  },
+  {
+    "revision": "0d2b78648d2fdaa42c2f4decbcd83d03",
+    "url": "/static/media/sleet.0d2b7864.png"
+  },
+  {
+    "revision": "cdced261369056c3699fb730f9d54659",
+    "url": "/static/media/snow.cdced261.png"
+  },
+  {
+    "revision": "273418751ac21ad3ed170a68a98b1ee6",
+    "url": "/static/media/sunny.27341875.png"
+  }
+]);
